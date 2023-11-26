f261ba7d82c4e0ae3e10dae2c23c6fbb2fbf5d9d
Markus Staub <markusstaub1.gmail.com@zbw-online.ch>
2023-11-26 22:14:32 +0100

    Projektdateien hinzufügen.

---
+++ DriveTogetherApp/Shared/Auto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DriveTogetherApp.Shared
+{
+    public class Auto
+    {
+        public int AutoId { get; set; } //  PK' 
+        public string Fahrtenanbieter { get; set; }
+        public string Marke {  get; set; }
+        public string Modell { get; set; }
+        public string Farbe { get; set; }
+        public string Kennzeichen { get; set; }
+        public DateTime Baujahr { get; set; }
+        public string Typ { get; set; }
+
+        // Navigationseigenschaften
+        //public virtual ICollection<Fahrt> Fahrten { get; set; }
+    }
+}
