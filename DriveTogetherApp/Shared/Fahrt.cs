2dfda254815ee884a357ab258f93d483b36ee012
Markus Staub <markusstaub1.gmail.com@zbw-online.ch>
2023-11-27 07:48:04 +0100

    Fartmodel hinzugefügt

---
+++ DriveTogetherApp/Shared/Fahrt.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DriveTogetherApp.Shared
+{
+    public class Fahrt
+    {
+        public int FahrtId { get; set; } //PK
+        public DateTime Startdatum { get; set; }
+        public DateTime Enddatum { get; set; }
+        public int AnzahlSitzplaetze { get; set; }
+        public decimal Preis { get; set; }
+
+        /*
+        // Foreign Keys
+        public int FahrtenanbieterId { get; set; }
+        public int AutoId { get; set; }
+
+        // Navigationseigenschaften
+        public virtual Auto Auto { get; set; }
+        public virtual ICollection<Buchung> Buchungen { get; set; }
+        */
+    }
+}
