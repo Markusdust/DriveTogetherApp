afc45d30f30919d4849c3051cf6da54368661408
Markus Staub <markusstaub1.gmail.com@zbw-online.ch>
2023-11-27 07:46:58 +0100

    Benutzermodel hinzugefügt

---
+++ DriveTogetherApp/Shared/Benutzer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DriveTogetherApp.Shared
+{
+    public class Benutzer
+    {
+        public int BenutzerId { get; set; } //PK
+        public string Vorname { get; set; }
+        public string Nachname { get; set; }
+        public string Email { get; set; }
+        public string Passwort { get; set; }
+        public DateTime Geburtsdatum { get; set; }
+        public string Telefonnummer { get; set; }
+        public DateTime Registrierungsdatum { get; set; }
+
+        /*
+        // Foreign Key
+        public int AdresseId { get; set; }
+
+        // Navigationseigenschaften
+        public virtual Adresse Adresse { get; set; }
+        public virtual ICollection<Feedback> Feedbacks { get; set; }
+        public virtual ICollection<Buchung> Buchungen { get; set; }
+        */
+    }
+}
