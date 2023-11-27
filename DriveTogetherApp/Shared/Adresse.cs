11603ee32a9a9b69c90f3cf6af6efa97b7841ae3
Markus Staub <markusstaub1.gmail.com@zbw-online.ch>
2023-11-27 07:50:43 +0100

    Adressemodel hinzugefügt

---
+++ DriveTogetherApp/Shared/Adresse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DriveTogetherApp.Shared
+{
+    public class Adresse
+    {
+        public int AdresseId { get; set; } //PK
+        public string Strasse { get; set; }
+        public string Hausnummer { get; set; }
+        public string PLZ { get; set; }
+        public string Stadt { get; set; }
+        public string Land { get; set; }
+
+        /*
+        // Navigationseigenschaften
+        public virtual ICollection<Benutzer> Benutzer { get; set; }
+        */
+    }
+
+}
