1ead20bc18bb7caeef803c77cb5155a089621bc3
Markus Staub <markusstaub1.gmail.com@zbw-online.ch>
2023-11-27 07:51:59 +0100

    Feedbackmodel hinzugefügt

---
+++ DriveTogetherApp/Shared/Feedback.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DriveTogetherApp.Shared
+{
+    public class Feedback
+    {
+        public int FeedbackId { get; set; } //PK
+        public string Kommentar { get; set; }
+        public DateTime FeedbackDatum { get; set; }
+
+        /*
+        // Foreign Keys
+        public int BenutzerId { get; set; }
+
+        // Navigationseigenschaften
+        public virtual Benutzer Benutzer { get; set; }
+        */
+    }
+}
