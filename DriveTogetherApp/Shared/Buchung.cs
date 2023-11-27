c9f7c0a882eb932d9574ff074095a53a7f781c56
Markus Staub <markusstaub1.gmail.com@zbw-online.ch>
2023-11-27 07:49:45 +0100

    Buchungmodel hinzugefügt

---
+++ DriveTogetherApp/Shared/Buchung.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DriveTogetherApp.Shared
+{
+    public class Buchung
+    {
+        public int BuchungId { get; set; } //PK
+        public DateTime Buchungsdatum { get; set; }
+        public string Zahlungsstatus { get; set; }
+        public string Zahlungsmethode { get; set; }
+
+        /*
+        // Foreign Keys
+        public int FahrtId { get; set; }
+        public int BenutzerId { get; set; }
+
+        // Navigationseigenschaften
+        public virtual Fahrt Fahrt { get; set; }
+        public virtual Benutzer Benutzer { get; set; }
+        */
+    }
+}
