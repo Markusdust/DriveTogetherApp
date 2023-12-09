98bb06318fb6c7645efeb7fb713167d3e705388d
Markus Staub <markusstaub1.gmail.com@zbw-online.ch>
2023-12-09 19:12:19 +0100

    createPasswordHash mit HMACSHA512 implementiert

--- DriveTogetherApp/Server/Services/AuthService/AuthService.cs
+++ DriveTogetherApp/Server/Services/AuthService/AuthService.cs
@@ -1,4 +1,5 @@
 ﻿using DriveTogetherApp.Shared.Model;
+using System.Security.Cryptography;
 
 namespace DriveTogetherApp.Server.Services.AuthService
 {
@@ -10,9 +11,21 @@ namespace DriveTogetherApp.Server.Services.AuthService
         {
             _context = context;
         }
-        public Task<ServiceResponse<int>> Register(Benutzer benutzer, string password)
+        public async Task<ServiceResponse<int>> Register(Benutzer benutzer, string password)
         {
-            throw new NotImplementedException();
+            if(await UserExists(benutzer.Email))
+            {
+                return new ServiceResponse<int> 
+                {
+                  Success = false,
+                  Message= "User already exists."  
+                };
+            }
+
+            CreatePasswordHash(password, out byte[] passswordHash, out byte[] passwortSalt);
+
+            benutzer.PasswortHash = passswordHash;
+            benutzer.PasswortSalt = passwortSalt;
         }
 
         public async Task<bool> UserExists(string email)
@@ -23,6 +36,16 @@ namespace DriveTogetherApp.Server.Services.AuthService
                 return true;
             }
             return false;  
+        }
+
+        private void CreatePasswordHash(string password , out byte[] passwordHash, out byte[] passwordSalt) 
+        {
+            using(var hmac = new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+
+            }
         }
     }
 }
