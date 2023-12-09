687a787a26502357558cdf789661b2dd9413466f
Markus Staub <markusstaub1.gmail.com@zbw-online.ch>
2023-12-09 19:20:12 +0100

    AuthController implementiert

---
+++ DriveTogetherApp/Server/Controllers/AuthController.cs
@@ -0,0 +1,32 @@
+﻿using DriveTogetherApp.Shared.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DriveTogetherApp.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+        [HttpPost("register")]
+        public async Task<ActionResult<ServiceResponse<int>>> Register(BenutzerRegister request)
+        {
+            var response = await _authService.Register(new Benutzer
+            {
+                Email = request.Email,
+            },
+            request.Password);
+
+            if(!response.Success)
+            {
+                return BadRequest(response);
+            }
+            return Ok(response);
+        }
+    }
+}
